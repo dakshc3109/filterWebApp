f146130e8e74be89dfc0a0ebc9fe95e81ed517f7
dakshc3109 <78908644+dakshc3109@users.noreply.github.com>
2021-08-29 13:46:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+  
+function preload(){
+
+}
+
+function setup(){
+    var canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('myFilter.png');
+}
